2be5d4eafa8c746ec006d99634eb25a30418e43c
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-02 12:51:23 -0400

    HiLo Game- Replay Logic Finished

--- HiLo/src/HiLo.java
+++ HiLo/src/HiLo.java
@@ -4,25 +4,33 @@ public class HiLo {
     public static void main(String[] args){
         //Definitions
         Scanner scan = new Scanner(System.in);
-        int ranNum = (int)(Math.random() * 100 + 1);
-        int guess = 0;
-        int numGuesses = 0;
+        String playAgain = "";
+        do{
+            int ranNum = (int)(Math.random() * 100 + 1);
+            int guess = 0;
+            int numGuesses = 0;
 
-        //Process
-        while(guess != ranNum){
-            System.out.println("Guess a number between 1 and 100:");
-            guess = scan.nextInt();
-            if(guess < ranNum){
-                numGuesses += 1;
-                System.out.println("Sorry, but that guess was too low.");
-            } else if (guess > ranNum){
-                numGuesses += 1;
-                System.out.println("Sorry, but that guess was too high.");
-            } else {
-                numGuesses += 1;
-                System.out.println("Congratulations! You win!");
-                System.out.println("It took you " + numGuesses + " guesses!");
+            //Guessing Logic
+            while(guess != ranNum){
+                System.out.println("Guess a number between 1 and 100:");
+                guess = scan.nextInt();
+                if(guess < ranNum){
+                    numGuesses += 1;
+                    System.out.println("Sorry, but " + guess + " is too low.");
+                } else if (guess > ranNum){
+                    numGuesses += 1;
+                    System.out.println("Sorry, but " + guess + " is  too high.");
+                } else {
+                    numGuesses += 1;
+                    System.out.println(guess + " is correct!");
+                    System.out.println("Congratulations! You win!");
+                    System.out.println("It took you " + numGuesses + " guesses!");
+                }
             }
-        }
+
+            //Replay Logic
+            System.out.println("Would you like to play again (y/n)?");
+            playAgain = scan.next();
+        } while (playAgain.equalsIgnoreCase("y"));
     }
 }
