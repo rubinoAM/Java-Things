991c5e534f976b33b2bf722e18782d72a99fbfe7
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-02 13:09:22 -0400

    MadLibs- Initializing Scanners

---
+++ JavaTheEasyWay/MadLibs/src/MadLibs.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MadLibs {
+    public static void main(String[] args){
+        //Blanks
+        Scanner sillyName = new Scanner(System.in);
+        Scanner unrealisticProfession = new Scanner(System.in);
+        Scanner country = new Scanner(System.in);
+        Scanner sillyNameTwo = new Scanner(System.in);
+        Scanner color = new Scanner(System.in);
+        Scanner adjective = new Scanner(System.in);
+        Scanner adverb = new Scanner(System.in);
+        Scanner sillyNameThree = new Scanner(System.in);
+        Scanner sillyNameFour = new Scanner(System.in);
+        Scanner facialFeature = new Scanner(System.in);
+        Scanner usCity = new Scanner(System.in);
+        Scanner sillyNameFive = new Scanner(System.in);
+        Scanner verb = new Scanner(System.in);
+        Scanner noun = new Scanner(System.in);
+        Scanner formerGreatActor = new Scanner(System.in);
+        Scanner nounTwo = new Scanner(System.in);
+    }
+}
