03898a9f1f7e25061efbdcaa48053221ea91d0e8
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-02 12:44:51 -0400

    HiLo Game- Guessing Logic Finished

--- HiLo/src/HiLo.java
+++ HiLo/src/HiLo.java
@@ -6,8 +6,23 @@ public class HiLo {
         Scanner scan = new Scanner(System.in);
         int ranNum = (int)(Math.random() * 100 + 1);
         int guess = 0;
+        int numGuesses = 0;
 
         //Process
-        System.out.println("Guess a number between 1 and 100:");
+        while(guess != ranNum){
+            System.out.println("Guess a number between 1 and 100:");
+            guess = scan.nextInt();
+            if(guess < ranNum){
+                numGuesses += 1;
+                System.out.println("Sorry, but that guess was too low.");
+            } else if (guess > ranNum){
+                numGuesses += 1;
+                System.out.println("Sorry, but that guess was too high.");
+            } else {
+                numGuesses += 1;
+                System.out.println("Congratulations! You win!");
+                System.out.println("It took you " + numGuesses + " guesses!");
+            }
+        }
     }
 }
