48d71a2d188110ae1fac8f4855fd97c2ca6fc25b
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-02 12:36:55 -0400

    HiLo Game

--- HiLo/src/HiLo.java
+++ HiLo/src/HiLo.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class HiLo {
+    public static void main(String[] args){
+        //Definitions
+        Scanner scan = new Scanner(System.in);
+        int ranNum = (int)(Math.random() * 100 + 1);
+        int guess = 0;
+
+        //Process
+        System.out.println("Guess a number between 1 and 100:");
+    }
 }
